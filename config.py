9386104137eca5a0914e290db01ec4a38677b896
team-m13 <75898962+team-m13@users.noreply.github.com>
2021-10-02 18:29:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 nameBot = 'Ева'
 url_news = 'https://ria.ru/lenta/'
 
-nameModel = "mode.pickle"
-nameVectoraizer = "modevectorizer.pickle"
+nameModel = "model.pickle"
+nameVectoraizer = "model_vectorizer.pickle"
 
 
 DOLLAR_RUB = 'https://www.banki.ru/products/currency/usd/'#'https://www.google.com/search?sxsrf=ALeKk01NWm6viYijAo3HXYOEQUyDEDtFEw%3A1584716087546&source=hp&ei=N9l0XtDXHs716QTcuaXoAg&q=%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&oq=%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80+&gs_l=psy-ab.3.0.35i39i70i258j0i131l4j0j0i131l4.3044.4178..5294...1.0..0.83.544.7......0....1..gws-wiz.......35i39.5QL6Ev1Kfk4'
